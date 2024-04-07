c3c706c71235455cef550a15abfc326dbc6b5f23
LHarveyDev <lucyanneharvey@hotmail.com>
2024-04-07 17:31:30 +0000

    heroku deployment

--- freedog/settings.py
+++ freedog/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-lharveydev-freedog-hmlybmmeiey.ws-eu110.gitpod.io']
+ALLOWED_HOSTS = ['freedog-92118a03f163.herokuapp.com', '8000-lharveydev-freedog-hmlybmmeiey.ws-eu110.gitpod.io']
 
 
 # Application definition
@@ -101,17 +101,20 @@ LOGIN_REDIRECT_URL = '/'
 
 WSGI_APPLICATION = 'freedog.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
