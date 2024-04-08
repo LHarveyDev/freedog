e1097a7f0d545c9ab516edfeaba0f00268bfdea1
LHarveyDev <lucyanneharvey@hotmail.com>
2024-04-08 20:53:16 +0000

    added cache control

--- freedog/settings.py
+++ freedog/settings.py
@@ -26,8 +26,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['freedog-92118a03f163.herokuapp.com', '8000-lharveydev-freedog-hmlybmmeiey.ws-eu110.gitpod.io']
-
+ALLOWED_HOSTS = [
+    'freedog-92118a03f163.herokuapp.com', '8000-lharveydev-freedog-hmlybmmeiey.ws-eu110.gitpod.io']    # noqa
 
 # Application definition
 
@@ -162,6 +162,12 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'freedog'
     AWS_S3_REGION_NAME = 'eu-west-2'
@@ -178,7 +184,6 @@ if 'USE_AWS' in os.environ:
     # Override static and media URLs in production
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
-    
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
